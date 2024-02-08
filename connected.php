74592e722ad8c32a7fff550a5b9bd9717a26de05
akli <difakli049@gmail.com>
2024-02-08 22:48:09 +0100

    connected page

---
+++ connected.php
@@ -0,0 +1,18 @@
+<?php require_once('inc/init.inc.php'); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="inc/css/style.css">
+    <title>GloGlo</title>
+</head>
+<body>
+    <div class="welcome-message-div">
+        <p class="welcome-message">Bienvenue <?php echo $_SESSION['username']?></p>
+    </div>
+    
+
+</body>
+</html>

f7bb880a5421b487d22c8c3cee6b2d1ac1ed6692
akli <difakli049@gmail.com>
2024-02-08 10:45:53 +0100

    add error messages

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
+<?php require_once('./inc/init.inc.php'); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./inc/css/style.css">
+    <link rel="stylesheet" href="inc/css/style.css">
     <title>GloGlo</title>
 </head>
 <body>
@@ -11,17 +12,28 @@
 
         <!-- load the logo with php script -->
         <div class="logo">
-            <img src="./inc/img/bird_2.jpg" alt="GloGlo">
+            <img src="./inc/img/bird_2.jpg" alt="GloGlo" style="width: 100px; height: 100px;">
         </div>
 
         <form action="./inc/process.php" method="post">
             <input type="text" name="username" placeholder="Username"> </br>
             <input type="password" name="password" placeholder="Password"></br>
+
+                <?php if (isset($_SESSION['input_data_error'])): ?>
+                    <p class="error-message"><?php echo $_SESSION['input_data_error']; ?></p>
+                    <?php unset($_SESSION['input_data_error']);?>
+                <?php endif; ?>
+                <?php if (isset($_SESSION['password_error'])): ?>
+                    <p class="error-message"><?php echo $_SESSION['password_error']; ?></p>
+                    <?php unset($_SESSION['password_error']);?>
+                <?php endif; ?>
             <div class="buttons">
                 <input type="submit" name="connect" value="Connect">
-                <input type="submit" name="reset" value="Reset">
-                <input type="submit" name="newAccount" value="New Account">
+                <input type="reset" name="reset" value="Reset">
+                <input type="button" name="newAccount" value="New Account" onclick="location.href='registration.php'">
             </div>
+            <!-- to prevent crsf -->
+            <input type="hidden" name="_token" value="<?php echo $_SESSION['_token'];?>">
         </form>
     </div>
 </body>

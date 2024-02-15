ea0c2deace6f119054aa82631e7ac5d700571c7d
akli <difakli049@gmail.com>
2024-02-15 23:50:05 +0100

    modify errors displays and env variables

--- inc/init.inc.php
+++ inc/init.inc.php
@@ -1,28 +1,46 @@
 <?php
+
+//--------- ERREURS
+ini_set('display_errors', 'Off');
+ini_set('log_errors', 'On');
+ini_set('error_log', 'error.log');
+
 //--------- CONNEXION BDD (mariaDB)
 // Database settings
 $db="my_db";
 $dbhost="localhost";
 $dbport=3306;
-$dbuser="akli";
-$dbpasswd="06101955";
+$dbuser = getenv('DB_USER');
+$dbpasswd = getenv('DB_PASS');
 
-$pdo = new PDO('mysql:host='.$dbhost.';port='.$dbport.';dbname='.$db.'', $dbuser, $dbpasswd);
-$pdo->exec("SET CHARACTER SET utf8");
-
-// die the connection if it fails
-if(!$pdo) {
-    die('Error: ' . $pdo->errorInfo());
+try {
+    $pdo = new PDO('mysql:host='.$dbhost.';port='.$dbport.';dbname='.$db.'', $dbuser, $dbpasswd);
+    $pdo->exec("SET CHARACTER SET utf8");
+} catch (PDOException $e) {
+    error_log('PDO Error: ' . $e->getMessage());
+    echo $dbuser;
+    echo $dbpasswd;
+    die('Database error');
 }
- 
+
 //--------- SESSION
+$secure = true; // to use secure connection
+$httponly = true; // to prevent access via JavaScript
+ini_set('session.cookie_secure', $secure);
+ini_set('session.cookie_httponly', $httponly);
+ini_set('session.use_only_cookies', 1);
 session_start();
- 
+
 //--------- CHEMIN
 define("RACINE_SITE","/var/www/html/");
- 
+
 //--------- VARIABLES
 $contenu = '';
- 
+
 //--------- AUTRES INCLUSIONS
-require_once("functions.inc.php");
+
+if (file_exists("inc/functions.inc.php")) {
+    require_once("inc/functions.inc.php");
+} else {
+    die("Error: functions.inc.php not found");
+}
